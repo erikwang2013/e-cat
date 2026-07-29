a8f59eb17d1c54f105544bc5bd40a5222c3f48cf
erik <erik@erik.xyz>
2026-07-29 11:59:48 +0800

    feat(logging): add tracing subscriber initialization

---
+++ ecat-logging/src/lib.rs
@@ -0,0 +1,17 @@
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+
+pub fn init(service_name: &str) {
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .with_target(true)
+        .with_level(true)
+        .compact();
+
+    let env_layer = tracing_subscriber::EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("info"));
+
+    tracing_subscriber::registry()
+        .with(env_layer)
+        .with(fmt_layer)
+        .init();
+}
